f919cabbfd2f93c4c50d7661fbca6699b1b5b40b
Aedin-ctrl <aedinlai@gmail.com>
2025-04-29 01:34:02 -0400

    Update part2.py

--- day_2/part2.py
+++ day_2/part2.py
@@ -3,25 +3,32 @@ imput_code = f.read()
 
 # part 2
 
-def check(list):
-    ...
+def check(lst):
+    difs = []
+    
+    for i in range(len(lst) - 1):
+        diff = int(lst[i+1]) - int(lst[i])
+        difs.append(diff)
+        
+    if all((1 <= num <= 3) or (-3 <= num <= -1) for num in difs):
+        if all(num > 0 for num in difs) or all(num < 0 for num in difs):
+            return True
     
 def main(imput_code):
     total = 0
     
-    for i in imput_code.split("\n"):
-        lst = i.split(" ")
+    for line in imput_code.strip().split("\n"):
+        lst = line.strip().split(" ")
         
-        difs = []
+        if check(lst):
+            total += 1
+        else:
+            for i in range(len(lst)):
+                new_list = lst[:i] + lst[i+1:]
+                if check(new_list):
+                    total += 1
+                    break 
         
-        for i in range(len(lst) - 1):
-            diff = int(lst[i+1]) - int(lst[i])
-            difs.append(diff)
-            
-        if all((1 <= num <= 3) or (-3 <= num <= -1) for num in difs):
-            if all(num > 0 for num in difs) or all(num < 0 for num in difs):
-                total += 1
-            
     return total
 
 print("part 2", main(imput_code))
