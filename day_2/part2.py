b243b7e6eca8c710bc9220adc52afac66a25f4ef
Aedin-ctrl <aedinlai@gmail.com>
2025-04-29 01:20:07 -0400

    Update part2.py

--- day_2/part2.py
+++ day_2/part2.py
@@ -1,19 +1,27 @@
 f = open("input.txt")
 imput_code = f.read()
 
+# part 2
 
+def check(list):
+    ...
+    
 def main(imput_code):
     total = 0
-    list1 = []
-    list2 = []
     
     for i in imput_code.split("\n"):
-        list1.append(i.split("   ")[0])
-        list2.append(i.split("   ")[1])
-    
-    for i in list1:
-        total += int(i) * (list2.count(i))
-    
+        lst = i.split(" ")
+        
+        difs = []
+        
+        for i in range(len(lst) - 1):
+            diff = int(lst[i+1]) - int(lst[i])
+            difs.append(diff)
+            
+        if all((1 <= num <= 3) or (-3 <= num <= -1) for num in difs):
+            if all(num > 0 for num in difs) or all(num < 0 for num in difs):
+                total += 1
+            
     return total
-         
+
 print("part 2", main(imput_code))
